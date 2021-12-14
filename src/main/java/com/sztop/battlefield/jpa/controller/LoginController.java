3caa1fcf1446f0ff131460948d2195751ac2ce36
szczepanTopolski <szczepanPL678@gmail.com>
2021-12-14 18:05:02 +0100

    Add example integration security test

--- src/main/java/com/sztop/battlefield/jpa/controller/LoginController.java
+++ src/main/java/com/sztop/battlefield/jpa/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.sztop.battlefield.jpa.controller;
 
 import com.sztop.battlefield.jpa.config.LoginCredentials;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,10 @@ public class LoginController {
     @PostMapping("/login")
     public void login(@RequestBody LoginCredentials credentials){
 
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "secured";
     }
 }
